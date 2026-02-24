18b2ac8a6099021511a9d5992552ed9c379ca161
Adityya Kaushal <adityyakaushal@gmail.com>
2026-02-24 18:39:41 -0500

    init: create repo

---
+++ astro.config.mjs
@@ -0,0 +1,77 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://uworbital.github.io',
+  base: '/docs/',
+  integrations: [
+    starlight({
+      customCss: [
+        './src/styles/custom.css',
+      ],
+      expressiveCode: {
+        themes: ['github-light-high-contrast', 'tokyo-night'],
+        removeUnusedThemes: true,
+        styleOverrides: {
+          // You can also override styles
+          borderRadius: '0.5rem',
+          borderColor: ['gray', 'black'],
+          frames: {
+            terminalTitlebarBackground: ['#414868', 'lightGray'],
+            terminalTitlebarBorderBottomColor: ['gray', 'black'],
+            terminalTitlebarDotsOpacity: '0.5',
+            terminalTitlebarForeground: ['white', 'black']
+          }
+        },
+      },
+      components: {
+        ThemeSelect: './src/components/ThemeSelect.astro',
+        Pagination: './src/components/Pagination.astro',
+      },
+      title: 'UW Orbital',
+      logo: {
+        src: './src/assets/icons/orbital_logo_long.png',
+        replacesTitle: true,
+      },
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/UWOrbital',
+        },
+        {
+          icon: 'discord',
+          label: 'Discord',
+          href: 'https://discord.gg/cw2gYKCTYJ',
+        }],
+      sidebar: [
+        {
+          label: 'Start Here',
+          autogenerate: { directory: 'start-here' },
+        },
+        {
+          label: 'Build Guides',
+          autogenerate: { directory: 'builds' },
+        },
+        {
+          label: 'Frontend Docs',
+          autogenerate: { directory: 'frontend' },
+        },
+        {
+          label: 'Backend Docs',
+          autogenerate: { directory: 'backend' },
+        },
+        {
+          label: 'Comms Guides',
+          autogenerate: { directory: 'comms' },
+        },
+        {
+          label: 'FreeRTOS Tasks',
+          autogenerate: { directory: 'tasks' },
+        },
+      ],
+    }),
+  ],
+});
